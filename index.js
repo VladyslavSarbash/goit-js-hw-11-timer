6b38a88caeec8f7c5417b57d94402dd3224031a7
VladyslavSarbash <79258970+VladyslavSarbash@users.noreply.github.com>
2021-08-13 20:13:02 +0300

    HW

--- index.js
+++ index.js
@@ -17,18 +17,20 @@ function timeIsNow() {
   const timeNow = Date.now() - countdownTimer.targetDate;
 
   const days = Math.floor(timeNow / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((timeNow % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const hours = Math.floor(
+    (timeNow % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
   const mins = Math.floor((timeNow % (1000 * 60 * 60)) / (1000 * 60));
   const secs = Math.floor((timeNow % (1000 * 60)) / 1000);
 
-  textContentTimer({days, hours, mins, secs})
+  textContentTimer({ days, hours, mins, secs });
 }
 
-function textContentTimer({days, hours, mins, secs}) {
-    daysValueRef.textContent = -days < 10 ? `0${-days}` : -days;
-    hoursValueRef.textContent = -hours < 10 ? `0${-hours}` : -hours;
-    minsValueRef.textContent = -mins < 10 ? `0${-mins}` : -mins;
-    secsValueRef.textContent = -secs < 10 ? `0${-secs}` : -secs;
+function textContentTimer({ days, hours, mins, secs }) {
+  daysValueRef.textContent = -days < 10 ? `0${-days}` : -days;
+  hoursValueRef.textContent = -hours < 10 ? `0${-hours}` : -hours;
+  minsValueRef.textContent = -mins < 10 ? `0${-mins}` : -mins;
+  secsValueRef.textContent = -secs < 10 ? `0${-secs}` : -secs;
 }
 
-setInterval(timeIsNow, 1000)
+setInterval(timeIsNow, 1000);
